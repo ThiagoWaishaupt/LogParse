a01f0114e018ff4ced3a96756f9a1fd796288a11
Thiago Waishaupt <thiagocais123@hotmail.com>
2019-09-15 01:03:08 -0300

    arquivos Spring Initializr

---
+++ src/main/java/com/api/logParse/LogParseApplication.java
@@ -0,0 +1,13 @@
+package com.api.logParse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogParseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogParseApplication.class, args);
+	}
+
+}
